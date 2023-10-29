1490d6c824b893158144d8093d279272d1c7f758
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2023-10-29 21:52:28 +0200

    created dir for specific lecture

--- 01_shop.py
+++
@@ -1,8 +0,0 @@
-class Shop:
-
-    def __init__(self, name: str, items: list):
-        self.name = name
-        self.items = items
-
-    def get_items_count(self) -> int:
-        return len(self.items)
